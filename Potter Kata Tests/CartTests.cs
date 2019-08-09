ed8bdadb729656c1cd44fcbfaf602584f4bf3657
Jake Apple <jakeapple2010@gmail.com>
2019-08-09 14:56:02 -0600

    Siddhart Solution

--- Potter Kata Tests/CartTests.cs
+++ Potter Kata Tests/CartTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Runtime.InteropServices;
 using NUnit.Framework;
 using Potter_Kata;
 
@@ -14,9 +16,61 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void No_books_returns_0()
         {
-            Assert.Fail();
+            var books = new int[] { };
+
+            var result = classUnderTest.CalculateCost(books);
+
+            Assert.AreEqual(0m, result);
+        }
+
+        [Test]
+        public void No_Books_Null_Returns_0()
+        {
+            var result = classUnderTest.CalculateCost(null);
+
+            Assert.AreEqual(0m, result);
+        }
+
+        [Test]
+        public void One_Book_Retuns_8()
+        {
+            var books = new int[] {1};
+
+            var result = classUnderTest.CalculateCost(books);
+
+            Assert.AreEqual(8m, result);
+        }
+
+        [Test]
+        public void Two_of_the_same_book_returns_16()
+        {
+            var books = new int[] {1, 1};
+
+            var result = classUnderTest.CalculateCost(books);
+
+            Assert.AreEqual(16m, result);
+        }
+
+        [Test]
+        public void Two_of_the_different_books_return_15_6()
+        {
+            var books = new int[] { 1, 2 };
+
+            var result = classUnderTest.CalculateCost(books);
+
+            Assert.AreEqual(15.6m, result);
+        }
+
+        [Test]
+        public void Three_of_the_different_books_returns_21_6()
+        {
+            var books = new int[] { 1, 2, 3 };
+
+            var result = classUnderTest.CalculateCost(books);
+
+            Assert.AreEqual(21.6m, result);
         }
     }
 }
